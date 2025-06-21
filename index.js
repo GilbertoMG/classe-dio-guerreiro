747475e4105fdf1913eaf98d0e273f1fe990e0c1
GilbertoMG <gilbmg@gmail.com>
2025-06-21 19:24:01 -0300

    Cria classe de guerreiro com testes de ataque

---
+++ index.js
@@ -0,0 +1,41 @@
+class Heroi {
+    constructor(nome, idade, tipo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+
+    atacar() {
+        let ataque;
+        switch (this.tipo) {
+            case 'mago':
+                ataque = 'usou magia';
+                break;
+            case 'guerreiro':
+                ataque = 'usou espada';
+                break;
+            case 'monge':
+                ataque = 'usou artes marciais';
+                break;
+            case 'ninja':
+                ataque = 'usou shuriken';
+                break;
+            default:
+                ataque = 'usou um ataque desconhecido';
+        }
+        console.log(`o ${this.tipo} atacou usando ${ataque}`);
+    }
+}
+
+
+const heroi1 = new Heroi('Gandalf', 1000, 'mago');
+heroi1.atacar();
+
+const heroi2 = new Heroi('Aragorn', 35, 'guerreiro');
+heroi2.atacar();
+
+const heroi3 = new Heroi('Lee Sin', 30, 'monge');
+heroi3.atacar();
+
+const heroi4 = new Heroi('Hanzo', 28, 'ninja');
+heroi4.atacar();
